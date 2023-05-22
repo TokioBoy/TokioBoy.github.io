3ac0076cd41d25baeeb5010117d24d39afe8edd1
Vlad <higemaster1@gmail.com>
2023-05-22 13:49:56 +0200

    vfvf

--- app.js
+++ app.js
@@ -11,47 +11,8 @@ window.addEventListener('scroll', function () {
       }
     }
   });
-  // ------------------------------------------------------------------------------------------------------------------------------
 
-  document.addEventListener('DOMContentLoaded', () => {
-    const scroll_to_about = document.querySelector('#scroll_to_about');
-    const scroll_to_timetable = document.querySelector('#scroll_to_timetable');
-    const scroll_to_location = document.querySelector('#scroll_to_location');
-    const scroll_to_contact = document.querySelector('#scroll_to_contact');
-    const scroll_to_registration = document.querySelector('#scroll_to_registration');
-    const about = document.querySelector('#about');
-    const timetable = document.querySelector('#timetable');
-    const location = document.querySelector('#location');
-    const contact = document.querySelector('#contact');
-    const registration = document.querySelector('#registration');
-
-    scroll_to_about.addEventListener('click', event => {
-      about.scrollIntoView({ 
-        behavior: 'smooth'
-      });
-    });
-    scroll_to_timetable.addEventListener('click', event => {
-      timetable.scrollIntoView({ 
-        behavior: 'smooth'
-      });
-    });
-    scroll_to_location.addEventListener('click', event => {
-      location.scrollIntoView({ 
-        behavior: 'smooth'
-      });
-    });
-    scroll_to_contact.addEventListener('click', event => {
-      contact.scrollIntoView({ 
-        behavior: 'smooth'
-      });
-    });
-    scroll_to_registration.addEventListener('click', event => {
-      registration.scrollIntoView({ 
-        behavior: 'smooth'
-      });
-    });
-  });
-  // ----------------------------------------------------------------------------------------------------------------------------------
+  // ----------------------------------------the_appearance_and_disappearance_of_sections--------------------------------------------------------------------------------
   mapboxgl.accessToken = 'pk.eyJ1IjoidG9raW9ib3kiLCJhIjoiY2w1ODIxMjV2MWRkeTNrcGJtbTdtdnhzciJ9.pPOEquxCJnl4OihAYBsBeQ';
 
   let loc = [14.433853676035787 , 50.087012541675264];
@@ -76,12 +37,12 @@ window.addEventListener('scroll', function () {
   .setLngLat(loc)
   .addTo(map)
   .setPopup(popup);
-  // ------------------------------------------------------------------------------------------------------------------------------------
-
+  // ----------------------------------------------------MAP--------------------------------------------------------------------------------------
   const speakers = document.querySelectorAll('.speaker');
 
-speakers.forEach((speaker) => {
+  speakers.forEach((speaker) => {
   speaker.addEventListener('click', () => {
     speaker.classList.toggle('active');
   });
 });
+  // ----------------------------------------------------Interaction_with_speakers--------------------------------------------------------------------------------------
